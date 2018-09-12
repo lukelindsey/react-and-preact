f25cad6f6c728c79bcfc9af7652b9a43a3444970
Luke Lindsey <luke.lindsey.dev@gmail.com>
2018-09-11 21:55:26 -0500

    Building just react

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  entry: {
+    ReactMain: "./src/index.tsx",
+    // PreactMain: "./src/PreactMain.tsx"
+  },
+  output: {
+    filename: "Scripts/generated/[name].js",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [{
+        enforce: "pre",
+        test: /\.js$/,
+        use: "source-map-loader"
+      },
+      {
+        enforce: "pre",
+        test: /\.tsx?$/,
+        use: "source-map-loader"
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    modules: [__dirname, "node_modules"]
+  },
+  plugins: [],
+  watch: false
+};
